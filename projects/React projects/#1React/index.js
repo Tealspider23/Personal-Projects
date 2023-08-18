044a7056bdaaa13c663079874b0d4872e39b8205
Tealspider23 <amitbehera204@gmail.com>
2023-08-18 19:35:51 +0530

    React initial commit#1

---
+++ projects/React projects/#1React/index.js
@@ -0,0 +1,56 @@
+
+
+//ReactDOM.render(<h1>Hello everyone!</h1>, document.getElementById('root'))
+
+//React code using ul and list items
+
+// ReactDOM.render(
+// <ul><li>Thing 1</li><li>Thing 2</li></ul>,
+//  document.getElementById('root')
+//  )
+
+
+//React code for navigation bar
+
+// function MainContent(){
+
+// return(
+//     <h1>
+//         I am learning React ! 
+//     </h1>
+
+// )
+// }
+
+// ReactDOM.render(
+//     <div>
+         
+//         <MainContent />
+    
+//     </div>,
+//     document.getElementById('root')
+// )
+
+// const page = (
+//     <div>
+//         <h1 className="header">This is JSX</h1>
+//         <p>This is a paragraph</p>
+//     </div>
+// )
+
+// const navbar = (
+//     <nav>
+//         <h1>tailor</h1>
+//         <ul><li>pricing</li>
+//         <li>About</li>
+//         <li>contact</li>
+//         </ul>
+//         </nav>
+// )
+
+
+// ReactDOM.render(
+//     navbar,
+//     document.getElementById("root")
+// )
+
