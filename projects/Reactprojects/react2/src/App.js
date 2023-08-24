1fb624f71ba65d0ff7936cc7858d8fe1d9efbdf6
Tealspider23 <amitbehera204@gmail.com>
2023-08-24 19:10:35 +0530

    Commit #7

--- projects/Reactprojects/react2/src/App.js
+++ projects/Reactprojects/react2/src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 // import ReactDOM from "react-dom"
 import Navbar from "./Component/navbar"
 import Main from "./Component/main"
+import "./styles.css"
 
 export default function App(){
     return(
