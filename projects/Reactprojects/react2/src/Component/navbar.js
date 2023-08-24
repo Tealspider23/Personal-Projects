cd93a155fcc241b850b399641e239457fcdb3d21
Tealspider23 <amitbehera204@gmail.com>
2023-08-24 08:03:13 +0530

    Commit #6

--- projects/Reactprojects/react2/src/Component/navbar.js
+++ projects/Reactprojects/react2/src/Component/navbar.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Navbar(){
 return(
-    <nav className="Nav23">
+    <nav>
         <img src = './logo192.png' alt = 'react logo' />
         <h3>ReactFacts</h3>
         <h4>React Course - Project 1</h4>
