4fff88bcea78ad62b31ca59c0e50011a27d4e3e8
Tealspider23 <amitbehera204@gmail.com>
2024-05-15 19:30:45 +0530

    Commit #2 for Chat-App

--- projects/Reactprojects/chat-app/src/components/ChatFeed.jsx
+++ projects/Reactprojects/chat-app/src/components/ChatFeed.jsx
@@ -9,6 +9,17 @@ const { chats , activeChat , userName , messages } = props;
 
 const chat = chats && chats[activeChat];
 
+const renderReadReceipts = ( message , isMyMessage) => chat.people.map((person,index) => person.last_read === message.id && (
+    <div
+    key = {'read_${index}'}
+    className="read-receipt"
+    style = {{
+        float: isMyMessage ? 'right' : 'left',
+        backgroundImage: person.person.avatar && 'url(${person.person.avatar})'
+    }}
+    />
+));
+
 const renderMessages = () =>{
     const keys = Object.keys(messages)
 
