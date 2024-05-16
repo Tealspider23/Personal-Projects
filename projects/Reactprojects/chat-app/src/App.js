6cc5a00b7523e1dd195e9c26b10a609e8d669d3e
Tealspider23 <amitbehera204@gmail.com>
2024-05-16 22:34:12 +0530

    Commit #3

--- projects/Reactprojects/chat-app/src/App.js
+++ projects/Reactprojects/chat-app/src/App.js
@@ -1,14 +1,17 @@
 import { ChatEngine } from 'react-chat-engine';
 import  ChatFeed  from './components/ChatFeed';
 import './App.css';
+import 'dotenv/config'
 
 const App = () =>{
     return(
         <ChatEngine
         height = "100vh"
-        projectID = "28a37c0e-f614-462c-af1f-4360fe5c3f89"
-        userName = "Amit"
-        userSecret = "bubun2004"
+        //take Project_ID value from .env file
+
+        projectID =(process.env.)
+        userName = "Amit" 
+        userSecret = "bubun2004" 
         renderChatFeed = {(chatAppProps) => <ChatFeed {...chatAppProps} /> }
         />
     )
