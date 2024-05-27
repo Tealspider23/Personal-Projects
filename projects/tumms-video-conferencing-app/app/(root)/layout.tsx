2d3a5357de8ef431e824396f649043e732a77be4
Tealspider23 <amitbehera204@gmail.com>
2024-05-27 16:51:46 +0530

    Commit #6 Meeting-Room completed Start-Instant Meeting completed Layout Metadata updated

--- projects/tumms-video-conferencing-app/app/(root)/layout.tsx
+++ projects/tumms-video-conferencing-app/app/(root)/layout.tsx
@@ -1,5 +1,14 @@
 import StreamVideoProvider from '@/providers/StreamClientProvider';
+import { Metadata } from 'next';
 import React, { ReactNode } from 'react'
+
+export const metadata: Metadata = {
+  title: "cOnvOz",
+  description: "Video calling app",
+  icons:{
+    icon:'icons/logo.svg'
+  },
+};
 
 const Rootlayout = ({children}:{children: ReactNode}) => {
   return (
